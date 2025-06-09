a1f801cc0a0a23d0a0bdbcf51c6f1b6553428e42
Vlad Kolesnikov <vladkol@google.com>
2025-06-09 10:00:09 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# Deploying Qwen 3 Embedding to Cloud Run 🚀
+
+Click the button below to deploy the [Qwen 3 Embedding](https://qwenlm.github.io/blog/qwen3-embedding/) service to Google [Cloud Run](https://console.cloud.google.com/run?utm_campaign=CDR_0xc245fc42_default_b423604648&utm_medium=external&utm_source=social) with [vLLM](https://docs.vllm.ai/).
+
+[![Open in Cloud Shell](https://gstatic.com/cloudssh/images/open-btn.svg)](https://shell.cloud.google.com/cloudshell/?terminal=true&show=terminal&cloudshell_git_repo=https://github.com/vladkol/qwen-3-enbedding-cloud-run&cloudshell_print=shell_print_command.txt&utm_campaign=CDR_0xc245fc42_default_b423604648&utm_medium=external&utm_source=social)
+
+The below performs the following operations:
+
+* Creates a Cloud Storage bucket for caching model files
+so that the model container doesn't have to download them
+from Hugging Face Hub every time the container starts.
+* Downloads model files to a temporary local cache.
+* Uploads model files to the Storage Bucket. Options `--no-clobber` doesn't let the command overwrite existing files.
+* Deploys Cloud Run service `qwen3-embedding` with the following notable parameters:
+    * Use NVIDIA L4 GPU (`--gpu-type=nvidia-l4` and `--gpu=1`)
+    * GCS bucket is mounted to `/model-cache` directory (`--add-volume...` options).
+    * Hugging Face hub cache is configured to `/model-cache` directory which the storage bucket is mounted to
+    (`HF_HOME` environment variable set via `--set-env-vars` option).
+    This way all service instances share the same Hugging Face cache data.
+    * Another environment variable `HF_HUB_OFFLINE=1` set in `--set-env-vars` option ensures that
+    vLLM doesn't re-download the model.
+    * Use a vLLM container image optimized for Google Cloud (`--image="us-docker.pkg.dev/vertex-ai/vertex-vision-model-garden-dockers/pytorch-vllm-serve:20250601_0916_RC01"`)
+    * Start vLLM's OpenAI compatible server with option `--task=embed` (for Embeddings API), and `--api-key` - to set a simple key-based authentication.
+
+> The recommended secure way to control access to Cloud Run services is to require IAM-based authentication
+(use `--no-allow-unauthenticated` rather than `--allow-unauthenticated`). Using `--api-key` option would be unnecessary in that case.
+Read more about it in [Cloud Run Authentication Overview](https://cloud.google.com/run/docs/authenticating/overview).
+
+## 🗒️ Disclaimers
+
+This is not an officially supported Google product. This project is not eligible for the [Google Open Source Software Vulnerability Rewards Program](https://bughunters.google.com/open-source-security).
+
+Code and data from this repository are intended for demonstration purposes only. It is not intended for use in a production environment.
---
+++ deploy.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PROJECT_ID="" # <-- Replace with your Google Cloud Project ID
+REGION="us-central1"
+
+HF_MODEL="Qwen/Qwen3-Embedding-0.6B"
+CUSTOM_API_KEY="ReplaceWithSecretText" # <-- Replace with your secret text
+SERVICE_NAME="qwen3-embedding-vllm"
+
+if [[ "${PROJECT_ID}" == "" ]]; then
+    PROJECT_ID=$(gcloud config get-value project -q)
+fi
+echo "Deploying to project ${PROJECT_ID}"
+
+pip install -U "huggingface_hub[cli]" -q
+
+set -e
+
+echo "Creating the service account if doesn't exist..."
+SERVICE_ACCOUNT="vllm-cloud-run-sa"
+SERVICE_ACCOUNT_ADDRESS="${SERVICE_ACCOUNT}@$PROJECT_ID.iam.gserviceaccount.com"
+sa_list=$(gcloud iam service-accounts list --quiet --format "value(email)" --project "${PROJECT_ID}" --filter "email:${SERVICE_ACCOUNT_ADDRESS}" 2>/dev/null)
+if [ -z "${sa_list}" ]; then
+    echo "Creating Service Account ${SERVICE_ACCOUNT}."
+    gcloud iam service-accounts create "${SERVICE_ACCOUNT}" \
+        --project "${PROJECT_ID}" \
+        --display-name="${SERVICE_ACCOUNT} - Cloud Run Service Account"
+fi
+
+BUCKET_NAME="huggingface-cache-${PROJECT_ID}-${REGION}"
+echo "Creating ${BUCKET_NAME} GCS bucket if doesn't exist..."
+gcloud storage buckets describe "gs://${BUCKET_NAME}" -q &> /dev/null || gcloud storage buckets create "gs://${BUCKET_NAME}" --location="${REGION}" --project="${PROJECT_ID}"
+echo "Granting the service account access to the bucket..."
+gcloud storage buckets add-iam-policy-binding "gs://${BUCKET_NAME}" --member="serviceAccount:${SERVICE_ACCOUNT_ADDRESS}" --role="roles/storage.objectViewer"
+
+echo "Downloading the model..."
+temp_dir=$(mktemp -d)
+huggingface-cli download "${HF_MODEL}" --cache-dir="${temp_dir}"
+
+echo "Uploading model files to the bucket..."
+gcloud storage cp --recursive --no-clobber "${temp_dir}/*" "gs://${BUCKET_NAME}/hub"
+rm -rf "${temp_dir}"
+
+echo "Deploying Cloud Run service..."
+gcloud beta run deploy "${SERVICE_NAME}" \
+    --image="us-docker.pkg.dev/vertex-ai/vertex-vision-model-garden-dockers/pytorch-vllm-serve:20250601_0916_RC01" \
+    --project="${PROJECT_ID}" \
+    --region="${REGION}" \
+    --service-account="${SERVICE_ACCOUNT_ADDRESS}" \
+    --add-volume name=model-cache-volume,type=cloud-storage,bucket=${BUCKET_NAME},readonly=true \
+    --add-volume-mount volume=model-cache-volume,mount-path=/model-cache \
+    --set-env-vars VLLM_LOGGING_LEVEL=WARNING,HF_HUB_OFFLINE=1,HF_HOME=/model-cache \
+    --command="python3" \
+    --args="-m,vllm.entrypoints.openai.api_server,--model=${HF_MODEL},--task=embed,--port=8080,--api-key=${CUSTOM_API_KEY}" \
+    --port=8080 \
+    --cpu=8 \
+    --memory=32Gi \
+    --gpu-type=nvidia-l4 \
+    --gpu=1 \
+    --max-instances=3 \
+    --no-gpu-zonal-redundancy \
+    --cpu-boost \
+    --no-cpu-throttling \
+    --allow-unauthenticated \
+    --timeout 1h
+
+SERVICE_URL=$(gcloud run services describe ${SERVICE_NAME} --project=${PROJECT_ID} --region=${REGION} --format="value(status.url)" -q)
+
+echo "Testing service ${SERVICE_URL}/v1/embeddings:"
+curl "${SERVICE_URL}/v1/embeddings" \
+    -H "Authorization: Bearer ${CUSTOM_API_KEY}" \
+    -H "Content-Type: application/json" \
+    -d "{ \"input\": [\"Hello world!\"], \"model\": \"${HF_MODEL}\" }"
+echo "Done!"
